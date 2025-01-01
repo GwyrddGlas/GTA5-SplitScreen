2a013dbc01b64a076bcd857ffe1c0b4dd8efd303
Lauryn <157061839+GwyrddGlas@users.noreply.github.com>
2025-01-01 15:57:15 +0000

    updated movement
    this is really fucky but works-ish

--- mutliplayer.lua
+++ mutliplayer.lua
@@ -1,6 +1,8 @@
+local natives = require("lib.natives2845")
+
 local players = {
-    {id = 1, name = "PlayerOne", pos = {}},
-    {id = 2, name = "PlayerTwo", pos = {}},
+    {id = 1, name = "PlayerOne", pos = {x = 0, y = 0, z = 0}},
+    {id = 2, name = "PlayerTwo", pos = {x = 0, y = 0, z = 0}},
 }
 
 local function drawText(text, x, y, scale)
@@ -15,17 +17,25 @@ end
 local toggle = false
 menu.create_thread(function()
     local key = {
-        purchase = MenuKey(),
+        wKey = MenuKey(),
+        aKey = MenuKey(),
+        sKey = MenuKey(),
+        dKey = MenuKey(),
     }
 
-    key.purchase:push_vk(0x58) --x keyboard
+    key.wKey:push_vk(0x57)  -- 'W' key
+    key.aKey:push_vk(0x41)  -- 'A' key
+    key.sKey:push_vk(0x53)  -- 'S' key
+    key.dKey:push_vk(0x44)  -- 'D' key
+    
     local lastPressedTime = 0
-    local cooldown = 500 
+    local cooldown = 500
+    local moveSpeed = 0.1  -- Adjust as necessary
 
     while true do
         drawText("Start SplitScreen Controller: back", 0.03, 0.75)
-        drawText("Enabled?: "..tostring(toggle), 0.5, 0.5)
-        
+        drawText("Enabled?: " .. tostring(toggle), 0.5, 0.5)
+
         controls.disable_control_action(0, 0, true) -- Switch Cam
         controls.disable_control_action(0, 31, true)  -- Move Left
         controls.disable_control_action(0, 32, true)  -- Move Right
@@ -37,12 +47,41 @@ menu.create_thread(function()
             toggle = not toggle
         end
 
-        if key.purchase:is_down() and (utils.time_ms() - lastPressedTime) >= cooldown then
+        -- WASD Controls using the corresponding virtual keys
+        if key.wKey:is_down() and (utils.time_ms() - lastPressedTime) >= cooldown then
             lastPressedTime = utils.time_ms()
-            
             local pos = player.get_player_coords(player.player_id())
-            pos.x = pos.x + 1
-            entity.set_entity_coords_no_offset(player.player_ped(), pos )
+            pos.y = pos.y - moveSpeed  -- Move Up (W)
+
+            -- Use TASK_TASK_GO_STRAIGHT_TO_COORD
+            natives.TASK.TASK_GO_STRAIGHT_TO_COORD(player.player_ped(), pos.x, pos.y, pos.z, moveSpeed, -1, 0.0, 0.0)
+        end
+
+        if key.aKey:is_down() and (utils.time_ms() - lastPressedTime) >= cooldown then
+            lastPressedTime = utils.time_ms()
+            local pos = player.get_player_coords(player.player_id())
+            pos.x = pos.x - moveSpeed  -- Move Left (A)
+
+            -- Use TASK_TASK_GO_STRAIGHT_TO_COORD
+            natives.TASK.TASK_GO_STRAIGHT_TO_COORD(player.player_ped(), pos.x, pos.y, pos.z, moveSpeed, -1, 0.0, 0.0)
+        end
+
+        if key.sKey:is_down() and (utils.time_ms() - lastPressedTime) >= cooldown then
+            lastPressedTime = utils.time_ms()
+            local pos = player.get_player_coords(player.player_id())
+            pos.y = pos.y + moveSpeed  -- Move Down (S)
+
+            -- Use TASK_TASK_GO_STRAIGHT_TO_COORD
+            natives.TASK.TASK_GO_STRAIGHT_TO_COORD(player.player_ped(), pos.x, pos.y, pos.z, moveSpeed, -1, 0.0, 0.0)
+        end
+
+        if key.dKey:is_down() and (utils.time_ms() - lastPressedTime) >= cooldown then
+            lastPressedTime = utils.time_ms()
+            local pos = player.get_player_coords(player.player_id())
+            pos.x = pos.x + moveSpeed  -- Move Right (D)
+
+            -- Use TASK_TASK_GO_STRAIGHT_TO_COORD
+            natives.TASK.TASK_GO_STRAIGHT_TO_COORD(player.player_ped(), pos.x, pos.y, pos.z, moveSpeed, -1, 0.0, 0.0)
         end
 
         system.wait(0)
